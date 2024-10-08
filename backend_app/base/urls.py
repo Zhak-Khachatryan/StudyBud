d126ad6e5a579a5c835393d452cba7a9abae241a
JakAsAlways <mrjak366@gmail.com>
2024-10-08 16:31:43 +0400

    Created the entire website. Making this commit as a backup if anything goes wrong after this.

---
+++ backend_app/base/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.login_page, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
+
+    path('', views.home, name='home'),
+    path('rooms/<str:pk>/', views.room, name='rooms'),
+    path('profile/<str:pk>', views.user_profile, name="user-profile"),
+
+    path('create-room/', views.create_room, name='create-room'),
+    path('update-room/<str:pk>/', views.update_room, name='update-room'),
+    path('delete-room/<str:pk>/', views.delete_room, name='delete-room'),
+    path('delete-message/<str:pk>/', views.delete_message, name='delete-message'),
+
+    path('topics/', views.topics_page, name='topics'),
+    path('activity/', views.activity_page, name='activity'),
+    path('update-user/', views.update_user, name='update-user'),
+]
